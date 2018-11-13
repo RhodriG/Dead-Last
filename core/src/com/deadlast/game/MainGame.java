d6286114e86da558f6645bd7edc8a3c7a79048a3
Xzytl <5693695+Xzytl@users.noreply.github.com>
2018-11-13 19:26:43 +0000

    Changed screen handling

--- core/src/com/deadlast/game/MainGame.java
+++ core/src/com/deadlast/game/MainGame.java
@@ -1,7 +1,8 @@
 package com.deadlast.game;
 
 import com.badlogic.gdx.Game;
-import com.deadlast.screens.MainMenuScreen;
+import com.deadlast.assets.Resources;
+import com.deadlast.screens.*;
 //import com.badlogic.gdx.Gdx;
 //import com.badlogic.gdx.graphics.GL20;
 //import com.badlogic.gdx.graphics.Texture;
@@ -9,9 +10,59 @@ import com.deadlast.screens.MainMenuScreen;
 
 public class MainGame extends Game {
 	
+	public Resources resources;
+	
+	private LoadingScreen loadingScreen;
+	private ScoreboardScreen scoreboardScreen;
+	private MenuScreen menuScreen;
+	private GameScreen gameScreen;
+	private HelpScreen helpScreen;
+	
+	public final static int MENU = 0;
+	public final static int SCOREBOARD = 1;
+	public final static int HELP = 2;
+	public final static int GAME = 3;
+	
 	@Override
 	public void create() {
-		setScreen(new MainMenuScreen(this));
+		loadingScreen = new LoadingScreen(this);
+		setScreen(loadingScreen);
+	}
+	
+	public void changeScreen(int screen) {
+		switch(screen) {
+		case MENU:
+			if (menuScreen == null) menuScreen = new MenuScreen(this);
+			this.setScreen(menuScreen);
+			break;
+		case SCOREBOARD:
+			if (scoreboardScreen == null) scoreboardScreen = new ScoreboardScreen(this);
+			this.setScreen(scoreboardScreen);
+			break;
+		case HELP:
+			if (helpScreen == null) helpScreen = new HelpScreen(this);
+			this.setScreen(helpScreen);
+			break;
+		case GAME:
+			if (gameScreen == null) gameScreen = new GameScreen(this);
+			this.setScreen(gameScreen);
+			break;
+		}
+	}
+	
+	@Override
+	public void render() {
+		super.render();
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
+	
+	@Override
+	public void pause() {
+		super.pause();
 	}
 //	SpriteBatch batch;
 //	Texture img;
