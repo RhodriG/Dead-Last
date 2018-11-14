55f022f264424133a63fcadb2d87e0b72948182d
Xzytl <5693695+Xzytl@users.noreply.github.com>
2018-11-14 02:24:34 +0000

    Updated screens

--- core/src/com/deadlast/screens/GameScreen.java
+++ core/src/com/deadlast/screens/GameScreen.java
@@ -1,24 +1,39 @@
 package com.deadlast.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.deadlast.controller.KeyboardController;
 import com.deadlast.game.MainGame;
+import com.deadlast.world.B2dModel;
 
 public class GameScreen extends DefaultScreen {
+	
+	B2dModel model;
+	private OrthographicCamera cam;
+	private Box2DDebugRenderer debugRenderer;
+	private KeyboardController controller;
 
 	public GameScreen(MainGame game) {
 		super(game);
-		// TODO Auto-generated constructor stub
+		cam = new OrthographicCamera(32, 24);
+		controller = new KeyboardController();
+		model = new B2dModel(controller);
+		debugRenderer = new Box2DDebugRenderer();
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-
+		Gdx.input.setInputProcessor(controller);
 	}
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
-
+		model.logicStep(delta);
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		debugRenderer.render(model.world, cam.combined);
 	}
 
 	@Override
--- core/src/com/deadlast/screens/MenuScreen.java
+++ core/src/com/deadlast/screens/MenuScreen.java
@@ -1,6 +1,5 @@
 package com.deadlast.screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
