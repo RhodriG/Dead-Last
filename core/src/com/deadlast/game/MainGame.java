20244345edf3911762ecc0d2771a3fc13af21d95
Xzytl <5693695+Xzytl@users.noreply.github.com>
2018-11-22 11:15:56 +0000

    Making important change to comment

--- core/src/com/deadlast/game/MainGame.java
+++ core/src/com/deadlast/game/MainGame.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-// main game class
+// Main game class
 public class MainGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
