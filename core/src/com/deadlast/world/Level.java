a91214e4835939f9f146540b6d223e5a19d720d8
Liam Devine <37180607+Fequjia@users.noreply.github.com>
2018-11-22 11:34:41 +0000

    Added level class
    Basic outline of level class

---
+++ core/src/com/deadlast/world/Level.java
@@ -0,0 +1,37 @@
+package com.deadlast.world;
+
+import com.badlogic.gdx.maps.Map;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * 
+ * @author ljd546
+ *
+ */
+public class Level {
+	
+	private World world;
+	private BodyFactory instance;
+	public Room[] rooms;
+	
+	public Level(World world) {
+		this.world = world;
+		this.instance = BodyFactory.getInstance(world);
+	}
+	
+	
+	
+	public class Room {
+		
+		public Map tiledMap;
+		public Body[] roomLayout;
+		
+		public void genLayout() {
+			
+		}
+		
+	}
+	
+}
