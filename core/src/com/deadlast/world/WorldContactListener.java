1532eb94562733cc30de43112fa7e2df4ac2156e
Xzytl <5693695+Xzytl@users.noreply.github.com>
2018-11-26 05:23:17 +0000

    Added sensor/detection check

--- core/src/com/deadlast/world/WorldContactListener.java
+++ core/src/com/deadlast/world/WorldContactListener.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
+import com.deadlast.entities.Entity;
+import com.deadlast.entities.Player;
 
 /**
  * Handles contact interactions with world bodies.
@@ -15,16 +17,34 @@ public class WorldContactListener implements ContactListener {
 	
 	@Override
 	public void beginContact(Contact contact) {
-		System.out.print("Contact: ");
+		System.out.println("Contact began!");
 		Fixture fA = contact.getFixtureA();
 		Fixture fB = contact.getFixtureB();
-		System.out.println(fA.getBody().getType() + " has hit " + fB.getBody().getType());
+		if (fA.getUserData() != null && fA.getBody().getUserData() instanceof Entity) {
+			// Alert entity to contact
+		}
+		if (fB.getUserData() != null && fB.getBody().getUserData() instanceof Entity) {
+			// Alert entity to contact
+		}
+		if (
+			(fA.isSensor() && fB.getBody().getUserData() instanceof Player) ||
+			(fB.isSensor() && fA.getBody().getUserData() instanceof Player)
+		) {
+			System.out.println("Player has entered detection range of enemy.");
+		}
 	}
 
 	@Override
 	public void endContact(Contact contact) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Contact ended!");
+		Fixture fA = contact.getFixtureA();
+		Fixture fB = contact.getFixtureB();
+		if (
+			(fA.isSensor() && fB.getBody().getUserData() instanceof Player) ||
+			(fB.isSensor() && fA.getBody().getUserData() instanceof Player)
+		) {
+			System.out.println("Player has left detection range of enemy.");
+		}
 	}
 
 	@Override
