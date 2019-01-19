ab1826065448b27d31e636a2018381ec062acc93
Xzytl <5693695+Xzytl@users.noreply.github.com>
2019-01-19 20:11:18 +0000

    Added PowerUp Builder and Factory

--- core/src/com/deadlast/entities/PowerUp.java
+++ core/src/com/deadlast/entities/PowerUp.java
@@ -1,5 +1,7 @@
 package com.deadlast.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -53,6 +55,71 @@ public class PowerUp extends Entity {
 	@Override
 	public void update(float delta) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public static class Builder {
+		
+		private DeadLast game;
+		private int scoreValue;
+		private Sprite sprite;
+		private float bRadius;
+		private Vector2 initialPos;
+		private Type type;
+		
+		public Builder setGame(DeadLast game) {
+			this.game = game;
+			return this;
+		}
+		
+		public Builder setScoreValue(int scoreValue) {
+			this.scoreValue = scoreValue;
+			return this;
+		}
+		
+		public Builder setSprite(Sprite sprite) {
+			this.sprite = sprite;
+			return this;
+		}
+		
+		public Builder setBodyRadius(float bRadius) {
+			this.bRadius = bRadius;
+			return this;
+		}
+		
+		public Builder setInitialPosition(Vector2 initialPos) {
+			this.initialPos = initialPos;
+			return this;
+		}
+		
+		public Builder setType(Type type) {
+			this.type = type;
+			return this;
+		}
+		
+		/**
+		 * Converts builder object into instance of PowerUP
+		 * @return an instance of PowerUp with the provided parameters
+		 * @throws IllegalArgumentException if mandatory parameters are not set
+		 */
+		public PowerUp build() {
+			if (game == null) {
+				throw new IllegalArgumentException("Invalid 'game' parameter");
+			}
+			if (type == null) {
+				throw new IllegalArgumentException("Invalid 'type' parameter");
+			}
+			if (sprite == null) {
+				sprite = new Sprite(new Texture(Gdx.files.internal("entities/blank_powerup.png")));
+			}
+			if (bRadius == 0) {
+				bRadius = 0.25f;
+			}
+			if (initialPos == null) {
+				throw new IllegalArgumentException("Invalid 'initialPos' parameter");
+			}
+			return new PowerUp(game, scoreValue, sprite, bRadius, initialPos, type);
+		}
 		
 	}
 
